4c6911fea47c593af55b1b6551352d2f5aa0772c
zh <779149549@qq.com>
2019-12-25 20:26:42 +0800

    NPOI文件导出示例

---
+++ Npoi_Sample/Npoi_Sample/ExcelHelper.cs
@@ -0,0 +1,79 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Npoi_Sample
+{
+    /// <summary>
+    /// Excel帮助类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ExcelHelper<T> where T : class
+    {
+        /// <summary>
+        /// 导出文件
+        /// </summary>
+        /// <param name="xlsName">文件名</param>
+        /// <param name="data">导出的数据源</param>
+        public static void SaveXlsChangeds(string xlsName, List<T> data)
+        {
+            IWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet("Sheet1");
+
+            var row = sheet.CreateRow(sheet.LastRowNum);
+
+            //创建标题行
+            int i = 0;
+            PropertyInfo[] propertyInfos = typeof(T).GetProperties();
+            foreach (var property in propertyInfos)
+            {
+                object[] objs = property.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (objs.Length > 0)
+                {
+                    //创建一个列并且赋值
+                    row.CreateCell(i).SetCellValue(((DescriptionAttribute)objs[0]).Description);
+                    i++;
+                }
+            }
+
+            //给数据填充到表格当中
+            int j = sheet.LastRowNum + 1, n = 0;
+            foreach (var item in data)
+            {
+                n = 0;
+                row = sheet.CreateRow(j++);
+                //获取一项的所有属性
+                var itemProps = item.GetType().GetProperties();
+                foreach (var itemPropSub in itemProps)
+                {
+                    var objs = itemPropSub.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                    if (objs.Length > 0)
+                    {
+                        row.CreateCell(n).SetCellValue(itemPropSub.GetValue(item, null) == null ? "" :
+                            itemPropSub.GetValue(item, null).ToString());
+                        n++;
+                    }
+                }
+            }
+
+            MemoryStream ms = new MemoryStream();
+
+            workbook.Write(ms);
+            ms.Flush();
+            ms.Position = 0;
+            FileStream fileStream = new FileStream(xlsName, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+            ms.WriteTo(fileStream);
+            ms.Close();
+            ms.Dispose();
+            fileStream.Close();
+            workbook.Close();
+        }
+    }
+}
---
+++ Npoi_Sample/Npoi_Sample/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Npoi_Sample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Student> stuLists = new List<Student>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 50; i++)
+            {
+                stuLists.Add(new Student()
+                {
+                    Id = i.ToString(),
+                    Name = "A" + i,
+                    Address = $"朝阳路{i}号",
+                    Sex = "男",
+                    Tel = "110"
+                });
+            }
+            gd.ItemsSource = stuLists;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excel表格(*.xls)|*.xls";
+            if (dialog.ShowDialog() == true)
+            {
+                ExcelHelper<Student>.SaveXlsChangeds(dialog.FileName, stuLists);
+            }
+        }
+    }
+
+    public class Student
+    {
+        [Description("序号")]
+        public string Id { get; set; }
+
+        [Description("姓名")]
+        public string Name { get; set; }
+
+        [Description("性别")]
+        public string Sex { get; set; }
+
+        [Description("地址")]
+        public string Address { get; set; }
+
+        [Description("电话")]
+        public string Tel { get; set; }
+    }
+}
